f28773d911250b7217e34c997ca647fc5c7c6cb0
umaimaamjad <157169085+umaimaamjad@users.noreply.github.com>
2024-02-02 02:54:09 +0500

    Add files via upload

---
+++ guessing game.py
@@ -0,0 +1,13 @@
+import random
+
+number = random.randint(1,100)
+guess=0
+
+while guess != number:
+     guess=int(input("Enter guess: "))
+     if (guess>number):
+              print("Guess lower!")
+     elif(guess < number):
+            print("Guess higher!")
+     else:
+        print("YOU WON!")
